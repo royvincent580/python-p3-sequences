262784aa575e3f6e45c96180ac7c45f830a18cfa
royvincent580 <vincentroy580@gmail.com>
2025-08-31 02:15:28 +0300

    seq

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    fib = [0, 1]
+    for i in range(2, length):
+        fib.append(fib[i-1] + fib[i-2])
+    
+    print(fib[:length])
